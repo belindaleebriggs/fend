606aaaa3184feedea838e8b3ca64ce29b05862fb
Belinda Briggs <>
2021-01-15 21:00:21 -0500

    feat add service workers

--- projects/evaluate-news-nlp/webpack.common.js
+++ projects/evaluate-news-nlp/webpack.common.js
@@ -2,6 +2,9 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
 
+
+const WorkboxPlugin = require('workbox-webpack-plugin');
+
 module.exports = {
     entry: './src/client/index.js',
     output: {
@@ -20,6 +23,7 @@ module.exports = {
         ]
     },
     plugins: [
+        new WorkboxPlugin.GenerateSW(),
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
--- projects/evaluate-news-nlp/webpack.prod.js
+++ projects/evaluate-news-nlp/webpack.prod.js
@@ -4,6 +4,8 @@ const common = require('./webpack.common.js')
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+
 module.exports = merge(common, {
     mode: 'production',
     module: {
@@ -15,9 +17,10 @@ module.exports = merge(common, {
         ]
     },
     plugins: [
+        new WorkboxPlugin.GenerateSW(),
         new MiniCssExtractPlugin({
             filename: "[name].css"
-        })
+        }),
     ],
     optimization: {
         minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
